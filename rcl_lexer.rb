5d3f63e251dcfca9647e55db1cf350d53b85fd64
sonota88 <yosiot8753@gmail.com>
2022-12-26 20:46:28 +0900

    refactor: if の扱いを改良 (rcl_lexer.rb)

--- rcl_lexer.rb
+++ rcl_lexer.rb
@@ -32,11 +32,6 @@ class RclLexer
         str = $1
         tokens << Token.new(:str, str, lineno)
         pos += str.size + 2
-      when /\A(if)[^a-z_]/ # TODO
-        str = $1
-        tokens << Token.new(:kw, "case", lineno)
-        tokens << Token.new(:kw, "when", lineno)
-        pos += str.size
       when /\A(-?[0-9]+)/
         str = $1
         tokens << Token.new(:int, str.to_i, lineno)
@@ -47,8 +42,13 @@ class RclLexer
         pos += str.size
       when /\A([A-Za-z_][A-Za-z0-9_]*\??)/
         str = $1
-        kind = KEYWORDS.include?(str) ? :kw : :ident
-        tokens << Token.new(kind, str, lineno)
+        if str == "if"
+          tokens << Token.new(:kw, "case", lineno)
+          tokens << Token.new(:kw, "when", lineno)
+        else
+          kind = KEYWORDS.include?(str) ? :kw : :ident
+          tokens << Token.new(kind, str, lineno)
+        end
         pos += str.size
       else
         p_e rest[0...100]
